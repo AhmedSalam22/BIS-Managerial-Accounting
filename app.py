ae9f1c235999bf031277b48d38c8f6e9169217d7
AhmedSalam22 <Ahmed0salam22@gmail.com>
2020-06-21 11:11:32 +0200

    Ahmed Maher 221999

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np
+
+
+
+st.title("Mangerial Accounting")
+
+
+
+
+
+
+st.markdown("#### Copyright@Ahmed Maher Fouy Mohamed Salam 2020")
